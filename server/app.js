57de7efd172caad1cc0dd43255a9d742dd2d9e6b
Heidi Tucholski <heiditucholski@gmail.com>
2026-03-06 14:38:45 -0500

    Ready for deployment

--- server/app.js
+++ server/app.js
@@ -3,18 +3,41 @@ const fs = require('fs');
 const app = express();
 
 app.use((req, res, next) => {
-// write your logging code here
-
+    const agent = req.headers['user-agent'].replace(/,/g, ';');
+    const logEntry = [
+        agent,
+        new Date().toISOString(),
+        req.method,
+        req.url,
+        'HTTP/'+req.httpVersion,
+        res.statusCode
+    ].join(',')+ '\n';
+    console.log(logEntry);
+    fs.appendFile('log.csv', logEntry, (err) => {
+        if (err) console.error('Error writing to log file:', err);
+    });
+    next();
 });
 
 app.get('/', (req, res) => {
-// write your code to respond "ok" here
-
+    res.status(200).send('ok');
 });
 
 app.get('/logs', (req, res) => {
-// write your code to return a json object containing the log data here
-
+    fs.readFile('log.csv', 'utf8', (err, data) => {
+        if (err) console.error('Error reading log file:', err);
+        
+        const rows = data.trim().split('\n').map(line => line.split(','));
+        const headers = rows[0];
+        const logEntries = rows.slice(1).map(row => {
+            const entry = {};
+            headers.forEach((header, i) => {
+                entry[header] = row[i];
+            });
+            return entry;
+        });
+        res.json(logEntries);
+    });
 });
 
 module.exports = app;
